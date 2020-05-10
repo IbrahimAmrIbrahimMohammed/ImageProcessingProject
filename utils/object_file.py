363f5b4ef11e66b0a009b18cc1a4e90303f7b476
lameesemad1996 <36021381+lameesemad1996@users.noreply.github.com>
2020-05-11 01:44:24 +0200

    Add object file

---
+++ utils/object_file.py
@@ -0,0 +1,30 @@
+'''
+@author ahmed hessuin
+description : this file contain every global object that is going to be used inside different files
+core file
+'''
+from utils import data_class as DC
+#======================================================================================================================#
+all_objects=[]
+object_id=0
+all_objects_as_dic={"/":[],"0":[],"1":[],"2":[],"3":[],"4":[],"5":[],"6":[],"7":[],"8":[],"9":[],"arrow head":[],"state condition":[],"loop back arrow":[],"state":[],"straight arrow":[],"curved arrow":[]}
+transaction=[]
+undo_all_objects=[]
+image=0
+image_copy=0
+image_test=0
+valid_verilog=False
+output_path=""
+image_name=""
+input_path=""
+output_path_xml=""
+model_1=""
+model_2=""
+model_3=""
+model_4=""
+model_5=""
+anchorDialogResult = ""
+anchorDialogChoice = ""
+#======================================================================================================================#
+
+
